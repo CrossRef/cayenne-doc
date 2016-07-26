08c28171e84de87687d8ff03a118c8e6ec6409ca
Karl Jonathan Ward <karl.j.ward@gmail.com>
2016-07-26 14:04:53 +0100

    Recombine because swagger  stuff is mostly broken

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar swagger2markup-cli-1.0.0.jar convert -i api.yaml -d out -c config.properties
+java -jar swagger2markup-cli-1.0.0.jar convert -i index.yaml -d out -c config.properties
