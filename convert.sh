bac06c50df835a46c3d7fd51d888f5a2a25b01cf
Karl Jonathan Ward <karl.j.ward@gmail.com>
2016-08-01 20:47:58 +0100

    Tweaks to the layout and hugo metadata

--- convert.sh
+++ convert.sh
@@ -6,4 +6,4 @@ java -jar swagger2markup-cli-1.0.0.jar convert -i index.yaml -d out -c config.pr
 # asciidoctor out/operations/*.adoc
 # asciidoctor out/*.adoc
 
-cat out/overview.md out/paths.md out/definitions.md > out/index.md
+cat metadata.toml out/overview.md out/paths.md out/definitions.md | sed s/definitions.md//g > out/index.md
