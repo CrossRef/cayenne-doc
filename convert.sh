50ce4f81e6a4e71a08730e34f7744cf37124f1b8
Karl Jonathan Ward <karl.j.ward@gmail.com>
2016-08-01 16:28:50 +0100

    Hugo layout
    Also switch back to markdown, since hugo can't generate ToCs for adoc

--- convert.sh
+++ convert.sh
@@ -6,4 +6,4 @@ java -jar swagger2markup-cli-1.0.0.jar convert -i index.yaml -d out -c config.pr
 # asciidoctor out/operations/*.adoc
 # asciidoctor out/*.adoc
 
-cat out/overview.adoc out/paths.adoc out/definitions.adoc > out/index.adoc
+cat out/overview.md out/paths.md out/definitions.md > out/index.md
