f78937728c791c1b1915ec9e739b6c5ac3a6a509
Karl Jonathan Ward <karl.j.ward@gmail.com>
2016-08-01 15:36:27 +0100

    Generate single adoc for inclusion in hugo site

--- convert.sh
+++ convert.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 java -jar swagger2markup-cli-1.0.0.jar convert -i index.yaml -d out -c config.properties
+
+# asciidoctor out/definitions/*.adoc
+# asciidoctor out/operations/*.adoc
+# asciidoctor out/*.adoc
+
+cat out/overview.adoc out/paths.adoc out/definitions.adoc > out/index.adoc
